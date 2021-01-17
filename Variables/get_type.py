785b5133ea940bf95a756d7ff0469eb067ab589c
Supaporn47 <supaporn.pum1@ku.th>
2021-01-17 13:24:56 +0700

    add get variable type

---
+++ Variables/get_type.py
@@ -0,0 +1,11 @@
+name = "supaporn pumcharoen"
+age = 23
+weight = 50
+can_walk = True
+can_swim = False
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_swim))
